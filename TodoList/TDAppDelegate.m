8f6575e125a049bce5d58fb153668832fa94ea26
Kevin Lacker <lacker@gmail.com>
2013-04-30 12:47:32 -0700

    fix copyright statements

--- TodoList/TDAppDelegate.m
+++ TodoList/TDAppDelegate.m
@@ -3,7 +3,6 @@
 //  TodoList
 //
 //  Created by Christine Yen on 11/1/12.
-//  Copyright (c) 2012 Parse. All rights reserved.
 //
 
 #import "TDAppDelegate.h"
--- TodoList/TDItemTableCellView.m
+++ TodoList/TDItemTableCellView.m
@@ -3,7 +3,6 @@
 //  TodoList
 //
 //  Created by Christine Yen on 11/13/12.
-//  Copyright (c) 2012 Parse. All rights reserved.
 //
 
 #import "TDItemTableCellView.h"
